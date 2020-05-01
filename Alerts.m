1549eb79032e0d951cca45626d02af3fcc332ea1
Thomas Tempelmann <tempelmann@gmail.com>
2020-05-02 01:59:10 +0200

    Alerts.m: Getting rid of warnings

--- Alerts.m
+++ Alerts.m
@@ -8,26 +8,28 @@
 
 #import "Alerts.h"
 
-
 @implementation Alerts
 
 @synthesize altButton;
 @synthesize otherButton;
 
--(void) doInfoAlertWithTitle:(NSString *)text 
-					infoText:(NSString *)info 
-				   forWindow:(NSWindow *)window 
-				withSelector:(SEL)sel 
-				withDelegate:(id)del 
-					runModal:(Boolean)modal
+- (void)doInfoAlertWithTitle:(NSString *)text
+                    infoText:(NSString *)info
+                   forWindow:(NSWindow *)window
+                withSelector:(SEL)sel
+                withDelegate:(id)del
+                    runModal:(Boolean)modal
 {
-	NSAlert *alert = [[NSAlert alertWithMessageText:text defaultButton:[NSString stringWithFormat:@"OK"] alternateButton:altButton
-									   otherButton:otherButton informativeTextWithFormat:info] retain];
-	
-	if( modal == NO )
-		[alert beginSheetModalForWindow:window modalDelegate:del didEndSelector:sel contextInfo:nil];
-	else
-		[alert runModal];
+    NSAlert *alert = [NSAlert alertWithMessageText:text
+                                     defaultButton:[NSString stringWithFormat:@"OK"]
+                                   alternateButton:altButton
+                                       otherButton:otherButton
+                         informativeTextWithFormat:@"%@", info];
+
+    if (modal == NO)
+        [alert beginSheetModalForWindow:window modalDelegate:del didEndSelector:sel contextInfo:nil];
+    else
+        [alert runModal];
 }
 
 @end
